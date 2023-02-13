81e64fb788da4290d25455242d5f188e90d13e8c
GreenHP <122482639+GreenHP@users.noreply.github.com>
2023-02-13 14:40:01 +0300

    Wall Jump fix & Camera controller
    INNOVANTIONS
    -Wall jump performance improved.
    (cant jump several times without dash)
    -Added smooth camera controller.
    -speed decreased. (9.5 -> 8.5)
    DEFICIENCIES
    -Player movement need's improvement.

---
+++ Brackey's Game Jam 2023.1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform Player; //the camera follow that object.
+    [SerializeField] private float dampTime = 0.4f; //camera move more smooth
+    private Vector3 cameraPos; //whic is where we want the camera move to
+    private Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        cameraPos = new Vector3(Player.position.x, Player.position.y, -10f);
+        transform.position = Vector3.SmoothDamp(gameObject.transform.position, cameraPos, ref velocity, dampTime);
+    }
+}
