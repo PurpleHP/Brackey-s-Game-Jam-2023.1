eb9f41c49cf40b51f48ac4808d0db0d8da24f9c3
PurpleHP <ermertay@gmail.com>
2023-02-15 03:59:49 +0300

    portal

--- Brackey's Game Jam 2023.1/Assets/Scripts/Dialogues.cs
+++ Brackey's Game Jam 2023.1/Assets/Scripts/Dialogues.cs
@@ -1,12 +1,32 @@
 /*
-1) Welcome to our game Player ... I'm that tutorial voice guy. I know my voice isn't the best but let's try to get along well with each other.
-
-2) Now you can move with A or D.
-
-3) Good
-
-4) 
+Notlar:
+(): Bu parantezdekileri farklı bir tonla söyler, fısıldayarak ya da hızlıca-anlaşılmadan.
+{}: Ayrıntı belirtir
+-: Bir olaydan sonra söylenecek sözler için.
 
 
+ Welcome to the game Player, It seems really lonely here. Just you and your mere body. I know that feeling, and I guess you know it too. 
+
+ Oh, you can't see anything, right? Don't panic but, here are your eyes... There you go. Now it's less lonely.
+
+ Did you try moving? The code might now work right now. Let me tell the developers to restart. Ok, now you can move with pressing A or D.
+
+ - Good
+
+ Those lazy developers also forgot to give you your jumping ability. Wait a second, alright done. Press Space to jump. You can hold it to go a little higher. And you can also do a wall jump.
+
+ Ok, look at least there are some more cool stuff. You can dash, press shift or x to dash.
+
+ That weird looking oval is a portal. It teleports you to your next experi.. {experiment diyecekti} level. Yeah, new level. It takes you to a new level.
+ 
+ As you can see, those red creatures will kill you (I know you got used to it {diye fısıldar}). Try avoiding them.
+
+ - How did you even die there?
+
+ - Umh, is this game that boring to make you go AFK.
+
+ Nice, this hell (I meant the game {hızlıca}), is a lot more fun right now haha.
+
+ You got the basics, good job. A lot faster than the previous one. 
 
 */
