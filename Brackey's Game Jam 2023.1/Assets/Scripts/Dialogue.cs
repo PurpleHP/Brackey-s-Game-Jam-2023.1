76aa65e58727b086df5af316f149514ce3cb850f
GreenHP <122482639+GreenHP@users.noreply.github.com>
2023-02-16 02:51:42 +0300

    Levels

--- Brackey's Game Jam 2023.1/Assets/Scripts/Dialogue.cs
+++ Brackey's Game Jam 2023.1/Assets/Scripts/Dialogue.cs
@@ -6,8 +6,11 @@ using TMPro;
 public class Dialogue : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI textDisplay;
+    [SerializeField] float[] dialogueTime;
     [SerializeField] string[] sentences;
     [SerializeField] float typingspeed;
+    [SerializeField] float counter = 0f;
+    private bool checkDia = true;
     private int index;
 
     void Start()
@@ -23,13 +26,29 @@ public class Dialogue : MonoBehaviour
             yield return new WaitForSeconds(typingspeed);
         }
     }
-    public void NextSentences()
+    private void Update()
     {
-        if(index < sentences.Length - 1)
+        counter += Time.deltaTime;
+
+        foreach (float a in dialogueTime)
+        {
+            if ((a < counter && a > counter - 0.8) && checkDia)
+            {
+                checkDia = false;
+                StartCoroutine(NextSentences());
+            }
+        }
+    }
+
+    IEnumerator NextSentences()
+    {
+        if (index < sentences.Length - 1)
         {
             index++;
             textDisplay.text = "";
             StartCoroutine(Type());
+            yield return new WaitForSeconds(1);
+            checkDia = true;
         }
     }
 }
