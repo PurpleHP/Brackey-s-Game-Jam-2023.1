526e0b9371169becb7fd417855ddb8fc45cc097a
PurpleHP <ermertay@gmail.com>
2023-02-18 18:16:53 +0300

    final level design

---
+++ Brackey's Game Jam 2023.1/Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuButton : MonoBehaviour
+{
+    // Start is called before the first frame update    [SerializeField] Image black;
+    [SerializeField] int goToLevel;
+    [SerializeField] Animator anim;
+    [SerializeField] Image black;
+
+    void Awake()
+    {
+        StartCoroutine(NextLevel());
+
+    }
+
+
+    IEnumerator NextLevel()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(()=>black.color.a == 1);
+        SceneManager.LoadScene(goToLevel);
+    }
+}
